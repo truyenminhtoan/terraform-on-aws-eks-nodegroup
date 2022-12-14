dd5afa1bc7e5ae6768e7cf7ecb678119e20b6c87
Toan Truyen <truyenminhtoan@gmail.com>
2022-12-14 17:09:05 +0700

    xray

--- 07-xray-manifests/c4-04-xray-configmap.tf
+++ 07-xray-manifests/c4-04-xray-configmap.tf
@@ -1,19 +1,13 @@
 # Resource: X-RAY ConfigMap
-/*
 resource "kubernetes_config_map_v1" "xray_config_configmap" {
   metadata {
     name      = "xray-config"
     namespace = kubernetes_namespace_v1.amazon_xray.metadata[0].name
   }
   data = {
-    "xrayconfig.json" = jsonencode({
-      "Socket" : {
-        "UDPAddress" : "0.0.0.0:2000",
-        "TCPAddress" : "0.0.0.0:2000"
-      },
-      "TotalBufferSizeMB" : 24,
-      "Version" : 2
-    })
+    "Version" : 2
+    "TotalBufferSizeMB" : 24,
+    "Socket.UDPAddress" : "0.0.0.0:2000"
+    "Socket.TCPAddress" : "0.0.0.0:2000"
   }
 }
-*/
--- 07-xray-manifests/c4-05-xray-daemonset.tf
+++ 07-xray-manifests/c4-05-xray-daemonset.tf
@@ -13,7 +13,7 @@ data "http" "get_xray_daemonset" {
 resource "kubectl_manifest" "xray_daemonset" {
     depends_on = [
       kubernetes_namespace_v1.amazon_xray,
-      #kubernetes_config_map_v1.xray_config_configmap,
+      kubernetes_config_map_v1.xray_config_configmap,
       kubectl_manifest.xray_serviceaccount
       ]
     yaml_body = data.http.get_xray_daemonset.response_body
