324e5b1d555d5b9509e9da949103d7347224cc03
Toan Truyen <truyenminhtoan@gmail.com>
2022-12-14 16:46:57 +0700

    xray

--- 07-xray-manifests/c4-04-xray-configmap.tf
+++ 07-xray-manifests/c4-04-xray-configmap.tf
@@ -1,18 +1,19 @@
 # Resource: X-RAY ConfigMap
-# resource "kubernetes_config_map_v1" "xray_config_configmap" {
-#   metadata {
-#     name      = "xray-config"
-#     namespace = kubernetes_namespace_v1.amazon_xray.metadata[0].name
-#   }
-#   data = {
-#     "xrayconfig.json" = jsonencode({
-#       "Socket" : {
-#         "UDPAddress" : "0.0.0.0:2000",
-#         "TCPAddress" : "0.0.0.0:2000"
-#       },
-#       "TotalBufferSizeMB" : 24,
-#       "Version" : 2
-#     })
-#   }
-# }
-
+/*
+resource "kubernetes_config_map_v1" "xray_config_configmap" {
+  metadata {
+    name      = "xray-config"
+    namespace = kubernetes_namespace_v1.amazon_xray.metadata[0].name
+  }
+  data = {
+    "xrayconfig.json" = jsonencode({
+      "Socket" : {
+        "UDPAddress" : "0.0.0.0:2000",
+        "TCPAddress" : "0.0.0.0:2000"
+      },
+      "TotalBufferSizeMB" : 24,
+      "Version" : 2
+    })
+  }
+}
+*/
